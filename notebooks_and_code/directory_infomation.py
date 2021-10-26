2076b6cf63fb33560fee4b82ead546ad425ee31a
AlexanderKroll <74175710+AlexanderKroll@users.noreply.github.com>
2021-10-26 16:37:30 +0300

    Moving BRENDA email address and password to environmental variables

--- notebooks_and_code/directory_infomation.py
+++ notebooks_and_code/directory_infomation.py
@@ -1,6 +1,9 @@
 #specify the directory that contains the folder with all datsets:
 datasets_dir = "C:\\Users\\alexk\\projects\\Prediction_of_KM_V3\\datasets\\"
 
+brenda_email_address = ""
+brenda_password = ""
+
 #specifying path with additional code:
 import sys
 sys.path.append('.\\additional_code')
